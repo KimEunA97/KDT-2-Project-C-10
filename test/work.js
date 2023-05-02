750e71475d4ef727fb63cf114e846d7e110646cf
kongukjae <kongukjae@gmail.com>
2023-05-02 16:50:51 +0900

    작업 맥락 확인 하기

---
+++ test/work.js
@@ -0,0 +1,54 @@
+/**
+ * * 1. tag를 만드는 함수를 써볼요량
+ * * 2. 스타일링도 간단하게 했음 좋겠음
+ * * 3. 문서에 구조로 들어갔으면 좋겠다.
+ * 
+ * * 4. 아직 구상중인거
+ * * - 스타일링이 조금 난감하다. 좋은사례가 있을까?
+ * * - 리액트는 스타일링을 어떻게 하지?
+ */
+
+
+
+
+function createTag() {
+  // pseudo : tag는 Element로 만들어졌음 좋겠음
+  // pseudo : createElement가 기억남
+  // pseudo : 스타일은 어떻게 했더라?
+
+  // * createTag에다가 스타일도 넣을까?
+  // * 스타일은 따로관리하자?
+
+  
+  // 뭐가 선언되어있음
+}
+
+let header, main, footer;
+
+const categoryText = ["오늘의 약", "타임라인", "검색", "설정"]
+const EditBtn = [["목록보기", "썸네일보기"], ["아침", "점심", "저녁"], ["새글쓰기", "플러스버튼"]];
+// 중요해 보인다. 함수콜을 배열에 받음
+const articleChild = [createTag('h1'), createTag('div')];
+
+
+
+for(array) {
+  // 스타일링을 하고 있음
+  // li tag 만들고 있음
+  createTag()
+}
+// ul li 만드는 작업
+
+array.fortEach(()=> {
+
+  element.forEach(text => {});
+
+});
+
+// 덧붙이는 작업
+for() {
+  // 
+}
+
+// 밑에는 다 덧붙임
+
