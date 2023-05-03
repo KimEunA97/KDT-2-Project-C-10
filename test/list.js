13ddeda4c41faed95a1b607f6d97030f588297c5
KimEunA97 <kimeuna9712@gmail.com>
2023-05-03 10:08:10 +0900

    가운데 정보 작업 진입

--- test/list.js
+++ test/list.js
@@ -55,22 +55,45 @@ for (let i = 0; i < articleChild.length; i++) {
   article.append(articleChild[i]);
 }
 
-console.dir(articleChild)
+
 //article > div > div
 //상단 : 약명정보, 화살표
 const pillSector = createTag('div');
-articleChild[1].append(pillSector);
+article.children[1].append(pillSector)
 
 //좌측 약명 컬럼
 const leftPillSector = createTag('div');
+pillSector.append(leftPillSector);
 
+//타이레놀 컨테이너
 const pillNameSector = createTag('div');
 leftPillSector.append(pillNameSector);
-
 const pillNameH2 = createTag('h2');
 pillNameSector.append(pillNameH2);
+pillNameH2.innerHTML = "타이레놀"
 
+//섬네일 컨테이너
 const pillThumnailSector = createTag('div');
-
+leftPillSector.append(pillThumnailSector);
 const pillThumnail = createTag('div');
 pillThumnailSector.append(pillThumnail);
+pillThumnail.innerHTML = "thumnail";
+
+//약 정보 컨테이너
+const pillDetailSector = createTag('div');
+leftPillSector.append(pillDetailSector)
+const pillDetailListContainer = createTag('ul');
+pillDetailSector.append(pillDetailListContainer);
+
+//약 정보, 내용
+const pillDetail = ["처방날짜:", "복용기간:", "용법, 용량:"]
+pillDetail.forEach(element => {
+
+  const pillDetailList = createTag('li');
+  pillDetailListContainer.append(pillDetailList);
+  pillDetailList.innerHTML = element
+
+});  
+
+const middleInfo = createTag('div')
+pillSector.append(middleInfo)
