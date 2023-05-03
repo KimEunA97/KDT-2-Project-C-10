8d6b9bdd07b2b8d86d31443b0d197bacfb84a7b3
KimEunA97 <kimeuna9712@gmail.com>
2023-05-03 10:43:09 +0900

    하단 증상 부분 작업완료

--- test/list.js
+++ test/list.js
@@ -57,7 +57,7 @@ for (let i = 0; i < articleChild.length; i++) {
 
 
 //article > div > div
-//상단 : 약명정보, 화살표
+//상단 : 약명, 정보, 화살표
 const pillSector = createTag('div');
 article.children[1].append(pillSector)
 
@@ -91,9 +91,37 @@ pillDetail.forEach(element => {
 
   const pillDetailList = createTag('li');
   pillDetailListContainer.append(pillDetailList);
-  pillDetailList.innerHTML = element
+  pillDetailList.innerHTML = element;
 
-});  
+});
 
+//가운데 정보
 const middleInfo = createTag('div')
-pillSector.append(middleInfo)
+pillSector.append(middleInfo);
+
+const middleInfoH3 = createTag('h3');
+middleInfo.append(middleInfoH3);
+middleInfoH3.innerHTML = "위장운동촉진제";
+const middleInfoDetail = createTag('div');
+middleInfo.append(middleInfoDetail);
+middleInfoDetail.innerHTML = "자몽주스, 오렌지 주스 및 사과주스 등과 복용하지 마세요."
+
+const pillArrow = createTag('div');
+pillSector.append(pillArrow);
+pillArrow.innerHTML = ">";
+
+
+//하단, 증상 부분
+const bottomTagBox = createTag('div');
+article.append(bottomTagBox);
+const SymptomSector = createTag('ul');
+bottomTagBox.append(SymptomSector);
+
+const symptomArr = ["증상1", "증상2", "증상3", "증상4", "증상5"]
+symptomArr.forEach(element => {
+
+  const symptom = createTag('li')
+  SymptomSector.append(symptom);
+  symptom.innerHTML = element;
+
+});
