4a1baa814968e89afc5c43b377566de4f11c43a5
KimEunA97 <kimeuna9712@gmail.com>
2023-05-08 12:20:42 +0900

    좌측 약 정보 export

--- test/article.js
+++ test/article.js
@@ -15,40 +15,7 @@ for (let i = 0; i < articleChild.length; i++) {
 
 
 //article > div > div
+//약 정보 전체 컨테이너
 //상단 : 약명, 정보, 화살표
 export const pillSector = createTag('div');
-article.children[1].append(pillSector)
-
-//좌측 약명 컬럼
-const leftPillSector = createTag('div');
-pillSector.append(leftPillSector);
-
-//타이레놀 컨테이너
-const pillNameSector = createTag('div');
-leftPillSector.append(pillNameSector);
-const pillNameH2 = createTag('h2');
-pillNameSector.append(pillNameH2);
-pillNameH2.innerHTML = "타이레놀"
-
-//섬네일 컨테이너
-const pillThumnailSector = createTag('div');
-leftPillSector.append(pillThumnailSector);
-const pillThumnail = createTag('div');
-pillThumnailSector.append(pillThumnail);
-pillThumnail.innerHTML = "thumnail";
-
-//약 정보 컨테이너
-const pillDetailSector = createTag('div');
-leftPillSector.append(pillDetailSector)
-const pillDetailListContainer = createTag('ul');
-pillDetailSector.append(pillDetailListContainer);
-
-//약 정보, 내용
-const pillDetail = ["처방날짜:", "복용기간:", "용법, 용량:"]
-pillDetail.forEach(element => {
-
-  const pillDetailList = createTag('li');
-  pillDetailListContainer.append(pillDetailList);
-  pillDetailList.innerHTML = element;
-
-});
+article.children[1].append(pillSector);
--- test/main.js
+++ test/main.js
@@ -1,7 +1,7 @@
 import { createTag } from "./function.js";
 
 
-export const main = createTag('main')
+export const main = createTag('main');
 export const article = createTag('article');
 
 export function mainPage() {
