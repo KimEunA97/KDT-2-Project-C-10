ef583122a25ec92da733eb8a661da6c96a687688
KimEunA97 <kimeuna9712@gmail.com>
2023-05-08 10:25:42 +0900

    모든 요소에 스타일 적용, margin 0 padding 0 boxSizing=border-box

--- test/list.js
+++ test/list.js
@@ -1,13 +1,6 @@
 import { createTag } from "./function.js";
 import { Allstyling } from './style.js'
-const allElements = document.body.getElementsByTagName("*");
 
-// 모든 요소에 스타일을 적용
-for (let i = 0; i < allElements.length; i++) {
-  allElements[i].style.margin = 0;
-  allElements[i].style.padding = 0;
-  allElements[i].style.boxSizing = "border-box";
-}
 
 
 const root = document.getElementById('root');
@@ -182,3 +175,10 @@ for (let i = 0; i < allUlTag.length; i++) {
 
 }
 
+const allElements = document.body.getElementsByTagName("*");
+// 모든 요소에 스타일을 적용
+for (let i = 0; i < allElements.length; i++) {
+  allElements[i].style.margin = 0;
+  allElements[i].style.padding = 0;
+  allElements[i].style.boxSizing = "border-box";
+}
