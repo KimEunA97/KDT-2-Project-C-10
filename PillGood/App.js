97d7af9c8a69e4effe0761d9be7d5f88f65c2fc6
KimEunA97 <kimeuna9712@gmail.com>
2023-05-23 00:39:04 +0900

    풀다운 메뉴 구조부터 만들기

--- PillGood/App.js
+++ PillGood/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import TabNavigation from './src/component/TabNavigation.js';
 
+import InteractionBtn from './src/component/InterationBtn.js';
+import { View } from 'react-native/types/index.js';
+
 export default function App() {
 
   return (
 
+    <View>
       <TabNavigation></TabNavigation>
-
-  );
+      <InteractionBtn></InteractionBtn>
+    </View>
+    
+    )
 }
---
+++ PillGood/src/component/InterationBtn.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TouchableOpacity, View } from "react-native/types";
+
+export default function InteractionBtn() {
+
+
+  return (
+
+    <View>
+      
+      <TouchableOpacity></TouchableOpacity>
+
+      <TouchableOpacity></TouchableOpacity>
+
+      <TouchableOpacity></TouchableOpacity>
+
+      <TouchableOpacity></TouchableOpacity>
+
+    </View>
+
+  )
+
+
+}
+
