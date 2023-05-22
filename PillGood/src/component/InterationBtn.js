e81aae59f5a8562c4f3b780cd3b7ebfd08e8cb66
KimEunA97 <kimeuna9712@gmail.com>
2023-05-23 00:41:30 +0900

    주석 달아서 구분하기. 컴포넌트 하나씩 진행

--- PillGood/src/component/InterationBtn.js
+++ PillGood/src/component/InterationBtn.js
@@ -7,13 +7,21 @@ export default function InteractionBtn() {
   return (
 
     <View>
-      
+
+
+      {/* 리스트형 레이아웃 */}
       <TouchableOpacity></TouchableOpacity>
 
+      {/* 앨범형 레이아웃 */}
       <TouchableOpacity></TouchableOpacity>
 
+      {/* 풀다운 */}
       <TouchableOpacity></TouchableOpacity>
 
+      {/* 편집모드 */}
+      <TouchableOpacity></TouchableOpacity>
+
+      {/* 생성버튼 */}
       <TouchableOpacity></TouchableOpacity>
 
     </View>
