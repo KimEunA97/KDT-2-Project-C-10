429b6addb4479f4f8aa7097aeb9530545aa7d25e
KimEunA97 <kimeuna9712@gmail.com>
2023-05-26 10:31:07 +0900

    create 메뉴 만들기

---
+++ PillGood/src/component/pillCreate.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View } from "react-native";
+
+
+
+export default function pillCreate() {
+
+  return (
+
+    <View>
+      <Text>asd</Text>
+    </View>
+
+  )
+
+}
+
+const styles = {
+
+  container: {
+
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    paddingTop : 3,
+
+
+  },
+  border: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "black",
+    padding : 5,
+
+  }
+
+}
--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Touchable } from 'react-native';
 
-export default function TodayPillMenu() {
+import pillCreate from '../component/pillCreate';
+
+
+const ComponentButton = () => {
+  const [componentList, setComponentList] = useState([]);
+
+  const handleClick = () => {
+    setComponentList([...componentList, <pillCreate key={componentList.length} />]);
+  };
+
   return (
     <View>
-      <Text>오늘의약</Text>
+      <button onClick={handleClick}>버튼</button>
+      {componentList.map((component, index) => (
+        <View key={index}>{component}</View>
+      ))}
     </View>
   );
 }
+
+export default ComponentButton;
