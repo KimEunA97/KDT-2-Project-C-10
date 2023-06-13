4b38e93c22313afb72ef3c7c0aaebba994093bd0
KimEunA97 <kimeuna9712@gmail.com>
2023-06-13 22:46:44 +0900

    모달 컴포넌트 만드는중, 스타일링 간단하게

---
+++ PillGood/src/component/modalBox.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Touchable, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+
+export default function ModalBox() {
+
+  return (
+
+    <View>
+      <Text>내용</Text>
+
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.confirmStyle}>
+          <Text style={styles.btnTextColor}>확인</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.cancelStyle}>
+          <Text style={styles.btnTextColor}>취소</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+
+  )
+}
+
+const styles = {
+
+  btnContainer: {
+
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "black"
+
+  },
+  confirmStyle: {
+
+    width: "30%",
+    border: "black",
+    backgroundColor: "blue"
+
+  },
+  cancelStyle: {
+    width: "30%",
+    border: "black",
+    backgroundColor: "red"
+  },
+  btnTextColor: {
+    color: "white"
+  }
+}
--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,89 +1,25 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, StyleSheet, TextInput, Modal } from 'react-native';
+import ModalBox from '../component/modalBox';
 
 export default function TodayPillMenu() {
 
-  const [visible, setVisible] = useState(false)
-
-  const modalVisible = () => {
-    setVisible(true);
-  }
-
   return (
-    <View style={styles.align}>
 
-      <TouchableOpacity
-        style={styles.border}
-        onPress={modalVisible}>
-        <Text>보이기</Text>
-      </TouchableOpacity>
-
-      <View>
-
-        <View style={styles.modalSize}>
-          <Modal
-            visible={visible}>
-            <View style={styles.modalContainer}>
-              <View style={styles.modalContent}>
-                <Text>내용</Text>
-
-                <View style={styles.btnContainer}>
-                  <TouchableOpacity>
-                    <Text>확인</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Text>취소</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </Modal>
-        </View>
-
-      </View>
+    <View>
+      <ModalBox></ModalBox>
     </View>
-  );
+
+  )
+
 }
 
 const styles = {
 
-  align: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  border: {
-    width: '80%',
-    height: '50%',
-    backgroundColor: "#336B66",
-    borderWidth: 1,
-    borderRadius: 2,
-    borderColor: "black",
-    padding: 5,
-    margin: 0,
+  container: {
 
-  },
-  modalSize: {
-
-    width : "50%",
-    height : "50%"
-
-  },
-  modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContent: {
-    backgroundColor: 'yellow',
-    padding: 20,
-    borderRadius: 4,
-    width: '80%',
-    height: '80%'
-  },
-  btnContainer: {
-    flex: 1,
-    justifyContent: "space-between",
-    flexDirection: "row",
-  },
+
+  }
 
 }
