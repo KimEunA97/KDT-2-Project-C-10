c0254b4896fba37fc2669bc1f096229ba9f768e3
KimEunA97 <kimeuna9712@gmail.com>
2023-06-13 21:50:23 +0900

    RN 기본 모듈 모달 써보기. 커스텀 하기 힘들어서 그냥 직접 만들어서 써야겠다고 판단. 혹시 모를 백업용 커밋

--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,12 +1,89 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet, TextInput } from 'react-native';
-
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, TextInput, Modal } from 'react-native';
 
 export default function TodayPillMenu() {
+
+  const [visible, setVisible] = useState(false)
+
+  const modalVisible = () => {
+    setVisible(true);
+  }
+
   return (
-    <View>
-      <Text>설정</Text>
+    <View style={styles.align}>
+
+      <TouchableOpacity
+        style={styles.border}
+        onPress={modalVisible}>
+        <Text>보이기</Text>
+      </TouchableOpacity>
+
+      <View>
+
+        <View style={styles.modalSize}>
+          <Modal
+            visible={visible}>
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                <Text>내용</Text>
+
+                <View style={styles.btnContainer}>
+                  <TouchableOpacity>
+                    <Text>확인</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Text>취소</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </View>
+
+      </View>
     </View>
   );
 }
 
+const styles = {
+
+  align: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  border: {
+    width: '80%',
+    height: '50%',
+    backgroundColor: "#336B66",
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "black",
+    padding: 5,
+    margin: 0,
+
+  },
+  modalSize: {
+
+    width : "50%",
+    height : "50%"
+
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    borderRadius: 4,
+    width: '80%',
+    height: '80%'
+  },
+  btnContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+
+}
