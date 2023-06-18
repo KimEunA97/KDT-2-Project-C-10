dd1e8e771a35aed544cd2c166e0fc41951ac5b05
KimEunA97 <kimeuna9712@gmail.com>
2023-06-18 16:41:17 +0900

    상태바 겹치는 증상 해결

--- PillGood/App.js
+++ PillGood/App.js
@@ -5,18 +5,21 @@ import { View, TouchableOpacity, Text, SafeAreaView } from 'react-native';
 import TabNavigation from './src/component/TabNavigation.js';
 import FixedStateButton from './src/pages/fixedStateButton.js';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <StatusBar style='blue'></StatusBar>
-      <TabNavigation></TabNavigation>
-      {/* footer */}
-      <FixedStateButton></FixedStateButton>
-    </View>
-
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          {/* header */}
+          <TabNavigation></TabNavigation>
+          {/* footer */}
+          <FixedStateButton></FixedStateButton>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
