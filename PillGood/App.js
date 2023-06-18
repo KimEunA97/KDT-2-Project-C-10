332ca8975d5a0056a0e7ee6d255a4e6bc87c48dc
KimEunA97 <kimeuna9712@gmail.com>
2023-06-18 16:38:25 +0900

    npm install react-native-safe-area-context 상태바 모듈 설치

--- PillGood/App.js
+++ PillGood/App.js
@@ -3,14 +3,17 @@ import { View, TouchableOpacity, Text, SafeAreaView } from 'react-native';
 
 
 import TabNavigation from './src/component/TabNavigation.js';
-import DropdownMenu from './src/component/pullDown.js';
 import FixedStateButton from './src/pages/fixedStateButton.js';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
+      {/* header */}
+      <StatusBar style='blue'></StatusBar>
       <TabNavigation></TabNavigation>
+      {/* footer */}
       <FixedStateButton></FixedStateButton>
     </View>
 
