5982714c2b2f3df9db69116465b3b55497b2433d
KimEunA97 <kimeuna9712@gmail.com>
2023-06-18 13:44:36 +0900

    타이머 입력칸 추가

--- PillGood/src/component/TodayModal.js
+++ PillGood/src/component/TodayModal.js
@@ -6,6 +6,9 @@ const TodayModal = ({ visible, onClose, onAddPill }) => {
   const [companyName, setCompanyName] = useState('');
   const [symptoms, setSymptoms] = useState('');
 
+  //타이머 제어
+  const [timerValue, setTimerValue] = useState('');
+
   const handlePillNameChange = (text) => {
     setPillName(text);
   };
@@ -18,18 +21,37 @@ const TodayModal = ({ visible, onClose, onAddPill }) => {
     setSymptoms(text);
   };
 
+  //타이머 설정 함수
+  const handleTimerChange = (text) => {
+    // parseInt는 문자열을 정수값으로 변환
+    const value = parseInt(text, 10);
+
+    if (!isNaN(value)) {
+      setTimerValue(value);
+    } else {
+      setTimerValue(0); // 숫자로 변환할 수 없는 경우 기본값으로 설정
+    }
+  };
+  
+  //pillList 컴포넌트로 전달
   const handleConfirm = () => {
+    // 객체로 여러 값을 전달할 수 있음
     if (pillName || companyName || symptoms) {
       const pill = {
-        pillName : pillName,
-        companyName : companyName,
-        symptoms : symptoms,
+        pillName: pillName,
+        companyName: companyName,
+        symptoms: symptoms,
+        timerValue : timerValue,
       }
+      //객체 콜백
       onAddPill(pill);
+      //확인버튼 누르면 입력한 칸 초기화
       setPillName('');
       setCompanyName('');
       setSymptoms('');
+      setTimerValue(0);
     }
+    //확인 누르면 닫기
     onClose();
   };
 
@@ -61,6 +83,16 @@ const TodayModal = ({ visible, onClose, onAddPill }) => {
             onChangeText={handleSymptomsChange}
           />
 
+
+          <Text style={styles.textColor}>시간 설정</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="시간 설정"
+            value={timerValue.toString()}
+            onChangeText={handleTimerChange}
+          />
+
+
           <View style={styles.btnContainer}>
             <TouchableOpacity onPress={handleConfirm} style={styles.createBtn}>
               <Text style={styles.textColor}>추가</Text>
@@ -88,22 +120,22 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 4,
     width: '80%',
-    height : "80%",
+    height: "80%",
     alignSelf: 'center',
   },
   textColor: {
     color: 'white',
     textAlign: 'center',
-    fontSize : 30,
+    fontSize: 30,
   },
   input: {
     borderWidth: 1,
-    backgroundColor : "white",
+    backgroundColor: "white",
     borderColor: 'gray',
     padding: 10,
     marginTop: 10,
-    fontSize : 20,
-    textAlign : "center"
+    fontSize: 20,
+    textAlign: "center"
   },
   btnContainer: {
     justifyContent: 'space-around',
@@ -113,12 +145,12 @@ const styles = StyleSheet.create({
   createBtn: {
     width: '45%',
     backgroundColor: '#4B73FF',
-    borderRadius : 5,
+    borderRadius: 5,
   },
   cancelBtn: {
     width: '45%',
     backgroundColor: '#00BC9A',
-    borderRadius : 5,
+    borderRadius: 5,
 
   },
 });
