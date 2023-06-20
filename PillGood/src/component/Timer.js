c72733792411945d099da6a061afa874bf831d9d
KimEunA97 <kimeuna9712@gmail.com>
2023-06-20 20:57:04 +0900

    timeValue 프롭스가 안쓰이고 있었음. 사용중인 코드로 변경. 여전히 렌더링 안됨.

--- PillGood/src/component/Timer.js
+++ PillGood/src/component/Timer.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
 
 const Timer = ({ timerValue }) => {
-  
-  //Date.now()는 현재 시간을 밀리초(millisecond)로 반환. 현재 시간을 나타냄.
-  // setCurrentTime(Date.now())와 같이 사용하면 currentTime 상태가 현재 시간으로 업데이트됨.
-  const [currentTime, setCurrentTime] = useState(Date.now());
+  const [remainingTime, setRemainingTime] = useState(timerValue);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentTime(Date.now());
+      setRemainingTime((prevTime) => Math.max(0, prevTime - 1000));
     }, 1000);
 
     return () => {
@@ -17,23 +14,17 @@ const Timer = ({ timerValue }) => {
     };
   }, []);
 
-  const formatTime = (timerValue) => {
-    if (timerValue <= 0) {
-      return "Expired";
-    }
-  
-    const remainingTime = Math.max(0, timerValue - currentTime);
-    const seconds = Math.floor(remainingTime / 1000) % 60;
-    const minutes = Math.floor(remainingTime / 1000 / 60) % 60;
-    const hours = Math.floor(remainingTime / 1000 / 60 / 60);
-  
+  const formatTime = (time) => {
+    const seconds = Math.floor(time / 1000) % 60;
+    const minutes = Math.floor(time / 1000 / 60) % 60;
+    const hours = Math.floor(time / 1000 / 60 / 60);
+
     return `${hours.toString().padStart(2, '0')}:${minutes
       .toString()
       .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
-  //요소 그룹화
-  return <Text>{formatTime(timerValue)}</Text>;
+  return <Text>{formatTime(remainingTime)}</Text>;
 };
 
 export default Timer;
