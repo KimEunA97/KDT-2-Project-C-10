37f959ae2e0ef6bfcd1ca3c8080011fa9dfe3360
KimEunA97 <kimeuna9712@gmail.com>
2023-06-22 21:52:06 +0900

    Alarm.js import 제외 하고 0초되면 바뀔 View부터 작업

--- PillGood/src/component/Timer.js
+++ PillGood/src/component/Timer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet } from 'react-native';
-import Alarm from './Alarm';
 
 const Timer = ({ timerValue }) => {
   const [remainingTime, setRemainingTime] = useState(timerValue);
