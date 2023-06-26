4b0adb6712c480b76aca350f2a0b1a8c3da1e65b
KimEunA97 <kimeuna9712@gmail.com>
2023-06-26 23:38:50 +0900

    URL에 한글이 16진수로 변환돼서 출력된다...

--- PillGood/src/api/api.js
+++ PillGood/src/api/api.js
@@ -7,7 +7,8 @@ const FetchAPIData = () => {
 
   useEffect(() => {
     // API 호출
-    axios.get('https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&type=json')
+    const pillName = "일동제약(주)"
+    axios.get('https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&type=json&ENTP_NAME= ' + pillName)
       .then(response => {
         setData(response.data);
       })
@@ -19,7 +20,7 @@ const FetchAPIData = () => {
   return (
     <View>
       {data ? (
-        <Text>{JSON.stringify(data.body.items[0].ENTP_NAME)}</Text>
+        <Text>{JSON.stringify(data)}</Text>
       ) : (
         <Text>Loading...</Text>
       )}
