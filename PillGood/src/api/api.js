c7430887c183ed44bbd564345eaf871c32f4f902
KimEunA97 <kimeuna9712@gmail.com>
2023-06-27 00:11:44 +0900

    URL을 인코딩한 문자열이었다. ASCII 코드가 16진수로 인코딩된 거였다.

--- PillGood/src/api/api.js
+++ PillGood/src/api/api.js
@@ -7,8 +7,11 @@ const FetchAPIData = () => {
 
   useEffect(() => {
     // API 호출
-    const pillName = "일동제약(주)"
-    axios.get('https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&type=json&ENTP_NAME= ' + pillName)
+    const pillName = "일동제약"
+    const hex = encodeURIComponent(pillName);
+    console.log(hex)
+    const url = 'https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&ENTP_NAME= ' + hex + '&type=json'
+    axios.get(url)
       .then(response => {
         setData(response.data);
       })
@@ -20,7 +23,7 @@ const FetchAPIData = () => {
   return (
     <View>
       {data ? (
-        <Text>{JSON.stringify(data)}</Text>
+        <Text>{JSON.stringify(data.body.items[1])}</Text>
       ) : (
         <Text>Loading...</Text>
       )}
