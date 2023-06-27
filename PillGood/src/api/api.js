25a9011dff90d9cacc25d7fc5ea646c604e03ef1
KimEunA97 <kimeuna9712@gmail.com>
2023-06-27 20:53:34 +0900

    백틱으로 넣으면 해결 된다.

--- PillGood/src/api/api.js
+++ PillGood/src/api/api.js
@@ -8,12 +8,13 @@ const FetchAPIData = () => {
   useEffect(() => {
     // API 호출
     const pillName = "일동제약"
-    const hex = encodeURIComponent(pillName);
-    console.log(hex)
-    const url = 'https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&ENTP_NAME= ' + hex + '&type=json'
+    const url = `https://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=3&ENTP_NAME=${pillName}&type=json`
+
+
     axios.get(url)
       .then(response => {
-        setData(response.data);
+        setData(response.data.items);
+        console.log(response.data.items.item)
       })
       .catch(error => {
         console.error(error);
@@ -23,7 +24,7 @@ const FetchAPIData = () => {
   return (
     <View>
       {data ? (
-        <Text>{JSON.stringify(data.body.items[1])}</Text>
+        <Text>{(data.body.items[1])}</Text>
       ) : (
         <Text>Loading...</Text>
       )}
