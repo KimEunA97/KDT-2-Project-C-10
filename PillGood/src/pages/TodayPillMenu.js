c683e2af300406286370b337a34fec71d9be63df
KimEunA97 <kimeuna9712@gmail.com>
2023-06-29 00:49:44 +0900

    올바른 로직 발견 . 이게 맞는것 같다.

--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,62 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView, Button } from 'react-native';
-import TodayModal from '../component/TodayModal';
-import PillList from '../component/PillList';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import RenderFetchPillData from '../api/RenderFetchPillData';
 import InputBox from '../api/Input';
 import ButtonBox from '../api/button';
 
 export default function TodayPillMenu({ onTimerDone }) {
-  // const [visible, setVisible] = useState(false);
-  // const [pillList, setPillList] = useState([]);
-
-  // const modalVisible = () => {
-  //   setVisible(true);
-  // };
-
-  // const closeModal = () => {
-  //   setVisible(false);
-  // };
-
-  // const addPill = (pill) => {
-  //   console.log(pill)
-  //   setPillList([...pillList, pill]);
-  // };
   const [inputValue, setInputValue] = useState('');
-  const [pillName, setPilName] = useState('');
-  
-  function handleSendValue(pillName) {
-    setPilName(pillName);
-  }
+  const [pillName, setPillName] = useState('');
+
+  const handleSendValue = () => {
+    setPillName(inputValue);
+  };
 
   return (
-    // <View style={styles.align}>
-    //   {pillList.length === 0 && (
-    //     <TouchableOpacity style={styles.button} onPress={modalVisible}>
-    //       <Image source={require('../img/plusMark.png')} style={styles.img}></Image>
-    //     </TouchableOpacity>
-    //   )}
-
-    //   <TodayModal visible={visible} onClose={closeModal} onAddPill={addPill} />
-
-    //   <PillList list={pillList} onTimerDone={onTimerDone} />
-
-    // </View>
-
     <View>
       <ScrollView>
-
-        {/* 전달 잘 됨. */}
         <RenderFetchPillData pillName={pillName} />
-        {/* ㅁㄴㅇ */}
-        <InputBox value={inputValue} onChange={setInputValue}></InputBox>
-        {/* ㅁㄴㅇ */}
-        <ButtonBox onClick={()=>handleSendValue(pillName)}></ButtonBox>
 
+        <InputBox value={inputValue} onChange={setInputValue} />
+        
+        <ButtonBox onClick={handleSendValue} />
       </ScrollView>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   align: {
