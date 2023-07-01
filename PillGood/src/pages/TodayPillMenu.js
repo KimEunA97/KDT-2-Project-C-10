9450bc7502cad22c4b21c4763129b12c04151aa8
KimEunA97 <kimeuna9712@gmail.com>
2023-07-01 17:43:02 +0900

    확인, 취소버튼 추가

--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, ScrollView, StyleSheet, onPress, TouchableOpacity, Modal } from 'react-native';
+import { View, ScrollView, StyleSheet, onPress, Text, TouchableOpacity, Modal } from 'react-native';
 import RenderFetchPillData from '../api/RenderFetchPillData';
 import InputBox from '../api/InputBox';
 import ButtonBox from '../api/button';
@@ -12,27 +12,59 @@ export default function TodayPillMenu({ }) {
   const handleSendValue = () => {
     setPillName(inputValue);
     setInvisible(false)
-
+    setInputValue('');
   };
-
+  const handleModalFalse = () => {
+    setInvisible(false)
+    setInputValue('');
+  }
   const pressTheCreateBtn = () => {
     setInvisible(true)
   }
 
-  return (
-    <View style={styles.align}>
-      <RenderFetchPillData pillName={pillName} />
-      <TouchableOpacity onPress={pressTheCreateBtn}
-        style={styles.button}></TouchableOpacity>
 
-      <Modal visible={invisible} >
-        <InputBox
-          value={inputValue}
-          onChange={setInputValue} />
-        <TouchableOpacity
-          onPress={handleSendValue}
-          style={styles.button}></TouchableOpacity>
+
+
+  return (
+    <View style={styles.allAlign}>
+      {/* 약정보 렌더링 */}
+      <RenderFetchPillData pillName={pillName} />
+
+      {/* Create 버튼 */}
+      <TouchableOpacity onPress={pressTheCreateBtn}
+        style={styles.createButton}>
+        </TouchableOpacity>
+
+      {/* 모달창 컨테이너 */}
+      <Modal visible={invisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {/* 입력칸 */}
+            
+            <InputBox
+              value={inputValue}
+              onChange={setInputValue}
+              style={styles.inputBoxStyle} />
+            <View style={styles.buttonContainer}>
+              {/* 확인버튼 */}
+              <TouchableOpacity
+                onPress={handleSendValue}
+                style={styles.confirmButton}>
+                <Text
+                  style={{ color: "white" }}>확인</Text>
+              </TouchableOpacity>
+              {/* 취소버튼 */}
+              <TouchableOpacity
+                onPress={handleModalFalse}
+                style={styles.confirmButton}>
+                <Text
+                  style={{ color: "white" }}>취소</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </Modal>
+
     </View>
   );
 }
@@ -40,13 +72,26 @@ export default function TodayPillMenu({ }) {
 
 
 const styles = StyleSheet.create({
-  align: {
+  allAlign: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#007088',
+    padding: 20,
     width: '80%',
-    height: '50%',
+    height: "80%",
+    alignSelf: 'center',
+  },
+  createButton: {
+    width: '80%',
+    height: '55%',
     backgroundColor: '#167286',
     borderWidth: 1,
     borderRadius: 2,
@@ -56,9 +101,33 @@ const styles = StyleSheet.create({
 
     alignItems: 'center'
   },
-  img: {
-    //이미지 크기 조절 방식
-    width: 40,
-    resizeMode: 'contain',
-  }
+
+  // 모달 내부
+  inputBoxStyle: {
+    width: "80%",
+    backgroundColor: "white"
+  },
+  buttonContainer: {
+    width: "80%",
+    height: "50%",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  confirmButton: {
+    width: '40%',
+    height: '20%',
+    backgroundColor: '#167286',
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: 'black',
+    padding: 5,
+    margin: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  // img: {
+  //   //이미지 크기 조절 방식
+  //   width: 40,
+  //   resizeMode: 'contain',
+  // }
 });
