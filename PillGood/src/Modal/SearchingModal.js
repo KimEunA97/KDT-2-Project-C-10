9a3d2ff7de1700f044a6ec4b10a634a5b380b967
KimEunA97 <kimeuna9712@gmail.com>
2023-07-02 13:45:59 +0900

    모달창 컴포넌트 나누기

---
+++ PillGood/src/Modal/SearchingModal.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, StyleSheet, Modal } from 'react-native';
+import NameWithInputBox from './NameWithInputBox';
+import ItsTimePicker from './ItsTimePicker';
+import UserPressButton from './UserPressButton';
+
+export default function SearchingModal({
+  handleSendValue,
+  handleModalFalse,
+  inputValue,
+  setInputValue,
+}) {
+  return (
+    <View style={styles.modalContainer}>
+      <View style={styles.modalContent}>
+
+        <NameWithInputBox
+          name="약이름 : "
+          value={inputValue}
+          onChangeText={setInputValue} />
+        <NameWithInputBox name="업체명 : " />
+        <NameWithInputBox name="증   상 : " />
+
+        <ItsTimePicker />
+
+        <View style={styles.buttonContainer}>
+          <UserPressButton
+            name="확인"
+            color="#00BC9A"
+            onPress={handleSendValue} />
+          <UserPressButton
+            name="취소"
+            color="#4B73FF"
+            onPress={handleModalFalse} />
+        </View>
+        
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalContent: {
+    flex: 0.6,
+    backgroundColor: '#007088',
+    padding: 30,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
--- PillGood/src/pages/TodayPillMenu.js
+++ PillGood/src/pages/TodayPillMenu.js
@@ -1,29 +1,28 @@
 import React, { useState } from 'react';
-import { View, Image, ScrollView, StyleSheet, onPress, Text, TouchableOpacity, Modal } from 'react-native';
+import { View, StyleSheet, Modal } from 'react-native';
 import RenderFetchPillData from '../api/RenderFetchPillData';
-import { ItsDateTimePicker } from '../component/TimePicker';
-import NameWithInputBox from '../Modal/NameWithInputBox'
-import UserPressButton from '../Modal/UserPressButton';
 import ListCreateButton from '../component/ListCreateButton';
-import ItsTimePicker from '../Modal/ItsTimePicker';
+import SearchingModal from '../Modal/SearchingModal'
 
-export default function TodayPillMenu({ }) {
+export default function TodayPillMenu() {
   const [inputValue, setInputValue] = useState('');
   const [pillName, setPillName] = useState('');
-  const [invisible, setInvisible] = useState(false)
+  const [invisible, setInvisible] = useState(false);
 
   const handleSendValue = () => {
     setPillName(inputValue);
-    setInvisible(false)
+    setInvisible(false);
     setInputValue('');
   };
+
   const handleModalFalse = () => {
-    setInvisible(false)
+    setInvisible(false);
     setInputValue('');
-  }
+  };
+
   const pressTheCreateBtn = () => {
-    setInvisible(true)
-  }
+    setInvisible(true);
+  };
 
   return (
     <View style={styles.allAlign}>
@@ -35,100 +34,20 @@ export default function TodayPillMenu({ }) {
 
       {/* 모달창 컨테이너 */}
       <Modal visible={invisible} transparent>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-
-            <NameWithInputBox name="약이름 : " />
-            <NameWithInputBox name="업체명 : " />
-            <NameWithInputBox name="증   상 : " />
-
-            <ItsTimePicker />
-
-            <View style={styles.buttonContainer}>
-              <UserPressButton
-                name="확인"
-                color="#00BC9A"
-                onPress={handleSendValue} />
-              <UserPressButton
-                name="취소"
-                color="#4B73FF"
-                onPress={handleModalFalse} />
-            </View>
-
-          </View>
-        </View>
+        <SearchingModal
+          handleSendValue={handleSendValue}
+          handleModalFalse={handleModalFalse}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
       </Modal>
-
-    </View >
+    </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   allAlign: {
     alignItems: 'center',
     justifyContent: 'center',
-
   },
-
-
-  //모달 스타일링
-  modalContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-  },
-  modalContent: {
-    flex: 0.6,
-    backgroundColor: '#007088',
-    padding: 30,
-    borderRadius: 10,
-  },
-
-
-
-  //아래부터 모달 내부 스타일링
-
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  // confirmButton: {
-  //   width: 120,
-  //   backgroundColor: '#4B73FF',
-  //   borderRadius: 10,
-  //   padding: 5,
-  //   margin: 0,
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-  // },
-  // confirmText: {
-  //   fontSize: 30,
-  //   color: "white",
-  //   fontWeight: "bold"
-  // },
-  // cancelButton: {
-  //   width: 120,
-  //   backgroundColor: '#00BC9A',
-  //   borderRadius: 10,
-  //   padding: 5,
-  //   margin: 0,
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-  // },
-  // cancelText: {
-  //   fontSize: 30,
-  //   color: "white",
-  //   fontWeight: "bold"
-  // },
-  // timePickerStyle: {
-  //   alignItems: "center",
-  //   marginTop: 25,
-
-  // }
-
 });
