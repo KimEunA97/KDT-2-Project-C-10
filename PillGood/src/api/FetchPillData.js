31d070c13e6ce3c986dc470716c6a2d096ca462b
KimEunA97 <kimeuna9712@gmail.com>
2023-07-02 18:57:57 +0900

    에러도 안뜨고 렌더링도 안됨 .

--- PillGood/src/api/FetchPillData.js
+++ PillGood/src/api/FetchPillData.js
@@ -14,7 +14,6 @@ export default function FetchPillData({ name }) {
     const fetchPillData = async () => {
       try {
 
-        const name = "타이레놀"
         const url = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=1XHlNsF6Hmgb8Wy1V%2FaTlJLpKD5korFpe6LEawoPDfjIHlM3RJSFRTgo%2BbGckpWv1t%2BS5VQj3%2FK51SWFgSq4oA%3D%3D&pageNo=1&numOfRows=3&itemName=${name}&type=json`;
         const response = await axios.get(url);
         setIsLoading(false);
@@ -45,16 +44,16 @@ export default function FetchPillData({ name }) {
   //     <Text>{data}</Text>
   //   )
   // }
-  // else if (data?.data.body?.data.body.totalCound > 0) {
-  //   const items = data.body.items;
-  //   return (
-  //     <View>
-  //       {
-  //         items.map((item, index) => {
-  //           <Text key={index}>{item.itemName}</Text>
-  //         })
-  //       }
-  //     </View>
-  //   )
-  // }
+  else if (data && data.body && data.body.totalCound > 0) {
+    const items = data.body.items;
+    return (
+      <View>
+        {
+          items.map((item, index) => {
+            <Text key={index}>{item.itemName}</Text>
+          })
+        }
+      </View>
+    )
+  }
 }
