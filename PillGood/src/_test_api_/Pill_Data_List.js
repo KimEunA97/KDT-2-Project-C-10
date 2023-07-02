059702550b802d358755886c6be9fc3af7d0aa84
KimEunA97 <kimeuna9712@gmail.com>
2023-07-02 10:27:09 +0900

    데이터 렌더링 컴포넌트 생성

--- PillGood/src/_test_api_/Pill_Data_List.js
+++ PillGood/src/_test_api_/Pill_Data_List.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { View } from "react-native";
 
 export default function PillDataList(props) {
 
   return (
 
-
+    <View>
+      <Text>{props}</Text>
+    </View>
 
   )
 
