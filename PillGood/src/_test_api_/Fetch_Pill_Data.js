ef30440ac90927fd17b0d19d01e1e703ca35f3b7
KimEunA97 <kimeuna9712@gmail.com>
2023-07-02 10:33:45 +0900

    어떻게 해야 할지 고민중

---
+++ PillGood/src/_test_api_/Fetch_Pill_Data.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+
+import axios from 'axios'
+
+const FetchPillData = () => {
+
+  const [pillData, setPillData] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com/todos/1"
+
+  axios.get()
+
+  return (
+
+
+
+  )
+
+}
